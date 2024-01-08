9e0af5c23e25968cc03fef6589cdd861cb86a962
Oldrado Junior <oldradojunior@gmail.com>
2024-01-07 21:33:34 -0300

    feat: add game component structure

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -2,9 +2,30 @@ import './styles.css';
 
 function Game({ verifyLetter }) {
   return (
-    <div>
-      <h1>Game</h1>
-      <button type="button" onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="word-container">
+        <span className="letter">A</span>
+        <span className="blank-square"></span>
+      </div>
+      <div className="letter-container">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button type="button">Jogar!</button>
+        </form>
+        <div className="wrong-letters-container">
+          <p>Letras já utilizadas:</p>
+          <span>a,</span>
+          <span>b,</span>
+        </div>
+      </div>
     </div>
   );
 }
