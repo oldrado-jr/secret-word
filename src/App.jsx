ab49118dc9d40f40e154274e6eba5028881be6df
Oldrado Junior <oldradojunior@gmail.com>
2024-01-08 21:42:17 -0300

    docs: add useful comments

--- src/App.jsx
+++ src/App.jsx
@@ -31,9 +31,11 @@ function App() {
   const [score, setScore] = useState(0);
 
   const pickWordAndCategory = useCallback(() => {
+    // pick a random category
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * categories.length)];
 
+    // pick a random word
     const word = words[category][Math.floor(Math.random() * words[category].length)];
 
     return { word, category };
@@ -53,9 +55,11 @@ function App() {
     setGameStage(stages[1].name);
   }, [pickWordAndCategory, stages]);
 
+  // handle letter input
   const verifyLetter = (letter) => {
     const normalizedLetter = letter.toLowerCase();
 
+    // check if guessed letter has already been used
     if (
       guessedLetters.includes(normalizedLetter)
       || wrongLetters.includes(normalizedLetter)
@@ -63,6 +67,7 @@ function App() {
       return;
     }
 
+    // add guessed letter or remove a chance
     if (letters.includes(normalizedLetter)) {
       setGuessedLetters((currentGuessedLetters) => [
         ...currentGuessedLetters,
@@ -78,32 +83,42 @@ function App() {
     }
   };
 
+  // restart the game
   const retry = () => {
     setScore(0);
     setGuesses(maxGuesses);
     setGameStage(stages[0].name);
   };
 
+  // clear letters state
   const clearLettersStates = () => {
     setGuessedLetters([]);
     setWrongLetters([]);
   };
 
+  // check if guesses ended
   useEffect(() => {
     if (guesses <= 0) {
+      // game ends and clear all states
       clearLettersStates();
+
       setGameStage(stages[2].name);
     }
   }, [guesses, stages]);
 
+  // check win condition
   useEffect(() => {
     const uniqueLetters = [... new Set(letters)];
 
+    // win contition
     if (
       gameStage === 'game'
       && guessedLetters.length === uniqueLetters.length
     ) {
+      // add score
       setScore((currentScore) => currentScore + 100);
+
+      // restart game with a new word
       startGame();
     }
   }, [letters, guessedLetters, gameStage, startGame]);
